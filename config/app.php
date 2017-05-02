e1d9fbcc69612500fe6a22987a00c64b113ca915
anisku11 <aan.faisalal@gmail.com>
2017-05-02 12:45:06 +0700

    Tambah Bahasa indonesia

--- config/app.php
+++ config/app.php
@@ -78,7 +78,7 @@ return [
 		    |
 	*/
 
-	'locale' => env('APP_LOCALE', 'en'),
+	'locale' => env('APP_LOCALE', 'id'),
 
 	/*
 		    |--------------------------------------------------------------------------
@@ -91,7 +91,7 @@ return [
 		    |
 	*/
 
-	'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
+	'fallback_locale' => env('APP_FALLBACK_LOCALE', 'id'),
 
 	/*
 		    |--------------------------------------------------------------------------
